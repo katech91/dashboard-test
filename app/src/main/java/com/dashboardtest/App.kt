597429ae708a73f660e4c835aaa2d58bedf21fce
Katerina Chernysheva <katech91@gmail.com>
2021-11-22 21:12:50 +0300

    prepare layouts

---
+++ app/src/main/java/com/dashboardtest/App.kt
@@ -0,0 +1,27 @@
+package com.dashboardtest
+
+import android.app.Application
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    private fun configureRetrofit() {
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+        val client: OkHttpClient = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://sandbox.skill-branch.ru/dashboards/grade")
+            .build()
+    }
+}
---
+++ app/src/main/java/com/dashboardtest/ui/costom/DashboardHelper.kt
@@ -0,0 +1,21 @@
+package com.dashboardtest.ui.costom
+
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Rect
+import android.graphics.RectF
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.PaintDrawable
+import android.graphics.drawable.shapes.Shape
+import com.dashboardtest.extensions.dpToPx
+import com.google.android.material.shape.MaterialShapeDrawable
+
+class DashboardHelper(
+    context: Context
+) {
+    private val radius: Float = context.dpToPx(8)
+//    private val background: MaterialShapeDrawable = MaterialShapeDrawable().apply {
+
+
+}
